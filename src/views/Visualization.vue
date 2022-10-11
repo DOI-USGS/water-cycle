c87c598997d114745ef4ce66702eb0a7fc15b7a5
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-10-11 16:30:00 -0500

    Vue setup

---
+++ src/views/Visualization.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="visualization">
+    <WaterCyle/>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Visualization',
+    components: {
+      WaterCyle: () => import( /* webpackPreload: true */ /*webpackChunkName: "section"*/ "./../components/WaterCycle")
+    },
+    computed: {
+    },
+    mounted(){
+      
+    },
+    methods:{
+    }
+} 
+</script>
+
+<style lang="scss">
+
+</style>
