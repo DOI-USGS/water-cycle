daae3b08828eef546bfadb59778c01ae836f8fea
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-11-04 12:05:55 -0500

    Adjust styling of options bar

--- src/components/WaterCycle.vue
+++ src/components/WaterCycle.vue
@@ -2,42 +2,44 @@
   <div id="content-container">
     <div
       id="button-container"
-      class="box row buttonContainer"
     >
-      <h3>
-        <span>
-          Visit the <a
-            href="https://www.usgs.gov/special-topics/water-science-school/science/water-cycle"
-            target="_blank"
-          >USGS Water Science School</a>
-        </span>
-        | 
-        <span>
-          <a
-            v-bind:href="downloadSite"
-            target="_blank"
-          >{{ currentLanguageDownloadText }}</a>
-        </span>
-        | 
-        <span>
-          Zoom:
-          <button
-            class="zoom button"
-            @click="$refs.zoomer.zoomIn()"
-          > + </button>
-          <button
-            class="zoom button"
-            @click="$refs.zoomer.zoomOut()"
-          > - </button>
-          | Language: 
-          <button
-            class="button"
-            :text="currentLanguageStatus"
-            @click="toggleLanguage"
-          >
-            {{ currentLanguageStatus }}
-          </button>
-        </span> 
+      <h3 class="optionsBar notButton">
+        <a
+          href="https://www.usgs.gov/special-topics/water-science-school/science/water-cycle"
+          target="_blank"
+        >Visit the Water Science School</a>
+      </h3>
+      <h3 class="optionsBar notButton"> | </h3>
+      <h3 class="optionsBar notButton">
+        <a
+          v-bind:href="downloadSite"
+          target="_blank"
+        >
+          {{ currentLanguageDownloadText }}
+        </a>
+      </h3>
+      <h3 class="optionsBar notButton"> | </h3>
+      <h3 class="optionsBar">
+        Language: 
+        <button
+          class="button"
+          :text="currentLanguageStatus"
+          @click="toggleLanguage"
+        >
+          {{ currentLanguageStatus }}
+        </button>
+      </h3>
+      <h3 class="optionsBar notButton"> | </h3>
+      <h3 class="optionsBar">
+        Zoom:
+        <button
+          class="zoom button"
+          @click="$refs.zoomer.zoomIn()"
+        > + </button>
+        <button
+          class="zoom button out"
+          @click="$refs.zoomer.zoomOut()"
+        > - </button>
       </h3>
     </div>
     <v-zoomer
@@ -46,7 +48,7 @@
       :aspect-ratio="imageAspectRatio"
       :max-scale="10"
       :zooming-elastic="false"
-      class="box row content"
+      class="content"
     >
       <picture>
         <source
@@ -122,8 +124,13 @@
 </script>
 
 <style scoped lang="scss">
+$diagramBlue: #016699;
+
 #image-zoomer {
   height: 88vh;
+  @media screen and (max-height: 900px) {
+    height: 84vh;
+  }
   @media screen and (max-height: 770px) {
     height: 80vh;
   }
@@ -131,42 +138,36 @@
     height: 85vh;
   }
   @media screen and (max-height: 500px) {
-    height: 80vh;
+    height: 50vh;
   }
   @media screen and (max-width: 400px) {
     height: 65vh;
   }
 }
+.optionsBar {
+  padding: 0.1em 0 0.1em 0;
+  margin-left: 0.5rem;
+}
 #button-container {
-  padding-left: 15px;
-  height: 100%;
-  z-index: 2;
-  --tw-bg-opacity: 1;
-  -webkit-touch-callout: none; /* iOS Safari */
-  -webkit-user-select: none; /* Safari */
-    -khtml-user-select: none; /* Konqueror HTML */
-      -moz-user-select: none; /* Old versions of Firefox */
-      -ms-user-select: none; /* Internet Explorer/Edge */
-          user-select: none; /* Non-prefixed version, currently
-                                supported by Chrome, Edge, Opera and Firefox */
+  padding-left: 0.5vw;
+  display: flex;
+  flex-wrap: wrap;
 }
 .button {
     border-radius: 0.25rem;
-    margin-right: 2px;
-    margin-top: 2px;
+    margin-top: 5px;
+    margin-bottom: 4px;
     padding: 2.5px 5px 2.5px 5px;
     max-width: 24rem;
     background-color: white;
     border: 0.5px solid #949494;
     border-radius: 0.25rem;
-    margin-left: 0.5rem;
-    margin-top: 0.5rem;
     -webkit-user-select: none; /* Safari */
     -ms-user-select: none; /* IE 10 and IE 11 */
     user-select: none; /* Standard syntax */
   }
   .button:hover {
-    background-color: #6E6E6E;
+    background-color: $diagramBlue;
     color: white;
     @media screen and (max-width: 600px) {
       background-color: white;
@@ -175,5 +176,18 @@
   }
   .button.zoom {
     padding: 2.5px 10px 2.5px 10px;
+  }
+  .button.zoom.out {
+    margin-left: 0.5rem;
+  }
+  .notButton {
+    margin-top: 7.5px;
+    margin-bottom: 4px;
+  }
+  a {
+    color: $diagramBlue;
+  }
+  a:hover {
+    font-weight: 700;
   }
 </style>
